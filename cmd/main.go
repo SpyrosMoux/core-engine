1d9c7fed3b6a9f8af4b6c2092482785c0fa989f4
Spyros Mouchlianitis <spirosgsaaa@gmail.com>
2024-09-28 23:13:22 +0300

    sdk dependent changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"github.com/spyrosmoux/api/pkg/business/pipelineruns"
+	"github.com/spyrosmoux/api/pkg/sdk"
+	"github.com/spyrosmoux/core-engine/internal/common"
 	"github.com/spyrosmoux/core-engine/internal/logger"
 	"github.com/spyrosmoux/core-engine/internal/pipelines"
 	"github.com/spyrosmoux/core-engine/internal/queue"
@@ -15,10 +18,18 @@ func main() {
 
 	var forever chan struct{}
 
+	client := sdk.NewClient(common.ApiBaseUrl)
+
 	go func() {
 		for d := range msgs {
 			logger.Log(logger.InfoLevel, "Received a message with correlation id: "+d.CorrelationId)
-			err := pipelines.RunPipeline(string(d.Body))
+
+			_, err := client.UpdatePipelineRunStatus(d.CorrelationId, pipelineruns.RUNNING)
+			if err != nil {
+				logger.Log(logger.ErrorLevel, "Failed to update pipeline with error: "+err.Error())
+			}
+
+			err = pipelines.RunPipeline(string(d.Body))
 			if err != nil {
 				logger.Log(logger.ErrorLevel, "Failed to run pipeline with error: "+err.Error())
 			}
@@ -27,6 +38,11 @@ func main() {
 			err = d.Ack(false)
 			if err != nil {
 				logger.Log(logger.ErrorLevel, "Failed to acknowledge message: "+err.Error())
+			}
+
+			_, err = client.UpdatePipelineRunStatus(d.CorrelationId, pipelineruns.COMPLETED)
+			if err != nil {
+				logger.Log(logger.ErrorLevel, "Failed to update pipeline with error: "+err.Error())
 			}
 		}
 	}()
